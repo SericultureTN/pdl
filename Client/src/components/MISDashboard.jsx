5d4bba75bd120cf4884c7e9fff347b36072322c4
Your Name <your-email@example.com>
2026-05-26 13:20:21 +0530

    chore: delete unused MISDashboard.jsx and MISEntryForm.jsx

--- Client/src/components/MISDashboard.jsx
+++
@@ -1,5 +0,0 @@
-import DFLsDistributionPage from "./mis/DFLsDistributionPage.jsx";
-
-export default function MISDashboard({ user, onBack }) {
-  return <DFLsDistributionPage user={user} onBack={onBack} />;
-}
